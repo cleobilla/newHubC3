a7bf68e7aeec31fa26cbc2dd638f9fff2ee7512a
Cleo Zanella Billa <cleo@Computador.local>
2020-04-24 15:32:29 -0300

    Adicionado o app.yaml e modificada a porta do servidor para deploy no Google Cloud.
    Adicionado o app.yaml e modificada a porta do servidor para deploy no Google Cloud.

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const pkg = require('./package')
 
 module.exports = {
   server: {
-    port: 8000, // default: 3000
+    port: process.env.PORT || 8000, // default: 3000
     host: '0.0.0.0', // default: localhost,
     timing: false
   },
